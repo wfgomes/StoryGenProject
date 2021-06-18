d0005b1de96bf31b039db41b7680ca30f0c92cae
Wesckley Gomes <wesckley.gomes@gmail.com>
2021-06-18 07:49:09 -0300

    separando os pesos

--- translation.py
+++ translation.py
@@ -12,14 +12,27 @@ vocab_size = 10000
 batch_size = 128
 
 def load_data_token(path, fname):
-    # post = []
+    post1 = []
+    post2 = []
+    post3 = []
+    post4 = []
     # with open('%s/%s.post' % (path, fname)) as f:
     #     for line in f:
     #         tmp = line.strip().split("\t")
-    #         post.append([p.lower().split() for p in tmp])
+    #         #post1.append([p.lower().split() for p in tmp])
+    #         post1 = [l.strip().lower().split() for l in tmp[0]]
 
     with open('%s/%s.post' % (path, fname), encoding="latin1") as f:
-        post = [line.strip().lower().split() for line in f.readlines()]
+        # post1 = [line.strip().lower().split("\t")[0].split() for line in f.readlines()]
+        # post2 = [line.strip().lower().split("\t")[1].split() for line in f.readlines()]
+        # post3 = [line.strip().lower().split("\t")[2].split() for line in f.readlines()]
+        # post4 = [line.strip().lower().split("\t")[3].split() for line in f.readlines()]
+        for line in f.readlines():
+            l = line.strip().lower().split("\t")
+            post1.append([l[0]])
+            post2.append([l[1]])
+            post3.append([l[2]])
+            post4.append([l[3]])
 
     with open('%s/%s.response' % (path, fname), encoding="latin1") as f:
         response = [line.strip().lower().split() for line in f.readlines()]
@@ -27,7 +40,7 @@ def load_data_token(path, fname):
     # for p, r in zip(post, response):
     #     data.append({'post': p, 'response': r})
 
-    return post, response
+    return post1, post2, post3, post4, response
 
 def truncate_pad(line, num_steps, padding_token):
     """Truncate or pad sequences."""
@@ -48,17 +61,20 @@ def build_array_nmt(lines, vocab, num_steps):
 
 def load_data_nmt(batch_size, num_steps, num_examples=600):
     """Return the iterator and the vocabularies of the translation dataset."""
-    source, target = load_data_token(data_dir, 'train')
+    source1, source2, source3, source4, target = load_data_token(data_dir, 'train')
     #Vocabulário único
-    vocab = utils.Vocab(source + target, min_freq=5,
+    vocab = utils.Vocab(source1 + source2 + source3 + source4 + target, min_freq=5,
                           reserved_tokens=['<pad>', '<bos>', '<eos>', '<unk>'])
 
     #tgt_vocab = utils.Vocab(target, min_freq=5,
     #                     reserved_tokens=['<pad>', '<bos>', '<eos>', '<unk>'])
-    src_array, src_valid_len = build_array_nmt(source, vocab, num_steps)
+    src_array1, src_valid_len1 = build_array_nmt(source1, vocab, num_steps)
+    src_array2, src_valid_len2 = build_array_nmt(source2, vocab, num_steps)
+    src_array3, src_valid_len3 = build_array_nmt(source3, vocab, num_steps)
+    src_array4, src_valid_len4 = build_array_nmt(source4, vocab, num_steps)
     tgt_array, tgt_valid_len = build_array_nmt(target, vocab, num_steps)
-    data_arrays = (src_array, src_valid_len, tgt_array, tgt_valid_len)
-    data_iter = utils.load_array(data_arrays, batch_size)
+    data_arrays = (src_array1, src_valid_len1, src_array2, src_valid_len2, src_array3, src_valid_len3, src_array4, src_valid_len4, tgt_array, tgt_valid_len)
+    data_iter = utils.load_array(data_arrays, batch_size, is_train=False)
     return data_iter, vocab, vocab
 
 #source, target = load_data_token(data_dir, 'train')
@@ -79,9 +95,9 @@ def load_data_nmt(batch_size, num_steps, num_examples=600):
 #     print('valid lengths for Y:', Y_valid_len)
 #     break
 
-embed_size, num_hiddens, num_layers, dropout = 200, 32, 2, 0.1
-batch_size, num_steps = 128, 60
-lr, num_epochs, device = 0.005, 1, utils.try_gpu()
+embed_size, num_hiddens, num_layers, dropout = 200, 512, 2, 0.1
+batch_size, num_steps = 128, 10
+lr, num_epochs, device = 0.005, 5, utils.try_gpu()
 
 train_iter, src_vocab, tgt_vocab = load_data_nmt(batch_size, num_steps)
 encoder = utils.Seq2SeqEncoder(len(src_vocab), embed_size, num_hiddens, num_layers,
--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ from mxnet import autograd, context, gluon, image, init, np, npx, nd
 from mxnet.gluon import nn, rnn
 import sys
 from d2l import mxnet as d2l
+import numpy as npy
 #import d2l as d2
 #d2l = sys.modules[__name__]
 
@@ -88,15 +89,22 @@ def train_seq2seq(net, data_iter, lr, num_epochs, tgt_vocab, device):
         timer = d2l.Timer()
         metric = d2l.Accumulator(2)  # Sum of training loss, no. of tokens
         for batch in data_iter:
-            X, X_valid_len, Y, Y_valid_len = [
-                x.as_in_ctx(device) for x in batch]
+            X1, X1_valid_len, X2, X2_valid_len, X3, X3_valid_len, X4, X4_valid_len, Y, Y_valid_len = [x.as_in_ctx(device) for x in batch]
             bos = np.array([tgt_vocab['<bos>']] * Y.shape[0],
                            ctx=device).reshape(-1, 1)
             dec_input = d2l.concat([bos, Y[:, :-1]], 1)  # Teacher forcing
             
             with autograd.record():
-                Y_hat, _ = net(X, dec_input, X_valid_len)
-                l = loss(Y_hat, Y, Y_valid_len)
+                Y_hat1, st1 = net(X1, dec_input, X1_valid_len)
+                l1 = loss(Y_hat1, X2, X2_valid_len)
+                Y_hat2, st2 = net(X2, dec_input, X2_valid_len)
+                l2 = loss(Y_hat2, X3, X3_valid_len)
+                Y_hat3, st3 = net(X3, dec_input, X3_valid_len)
+                l3 = loss(Y_hat3, X4, X4_valid_len)
+                Y_hat4, st4 = net(X4, dec_input, X4_valid_len)
+                l4 = loss(Y_hat4, Y, Y_valid_len)
+                l = l1 + l2 + l3 + l4
+
             l.backward()
             d2l.grad_clipping(net, 1)
             num_tokens = Y_valid_len.sum()
